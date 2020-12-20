7514ee2cdd7e1eeab176df4a45c53238e092f449
Martin Trejo Chavez <chillicoder@gmail.com>
2020-12-19 23:16:43 -0600

    Agrega soporte a MySQL

--- src/test/java/com/bezkoder/spring/data/jpa/test/JPAUnitTest.java
+++ src/test/java/com/bezkoder/spring/data/jpa/test/JPAUnitTest.java
@@ -5,6 +5,8 @@ import static org.assertj.core.api.Assertions.assertThat;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -14,6 +16,7 @@ import com.bezkoder.spring.data.jpa.test.repository.TutorialRepository;
 
 @RunWith(SpringRunner.class)
 @DataJpaTest
+@AutoConfigureTestDatabase(replace = Replace.NONE)
 public class JPAUnitTest {
 
   @Autowired
